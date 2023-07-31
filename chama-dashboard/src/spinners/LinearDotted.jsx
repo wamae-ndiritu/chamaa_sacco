fb06a173c4302bdb2040c21028e743c31ae45cbf
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-07-31 13:42:55 +0300

    Create Loading Spinner component

---
+++ chama-dashboard/src/spinners/LinearDotted.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./linearDotted.css";
+
+const LinearDotted = () => {
+  return (
+    <div className="linear-dotted-spinner">
+      <div className="dot dot-1"></div>
+      <div className="dot dot-2"></div>
+      <div className="dot dot-3"></div>
+    </div>
+  );
+};
+
+export default LinearDotted;
