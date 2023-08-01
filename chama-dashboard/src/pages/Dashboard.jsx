06dcdf640ff39812534a3aeab0756ed8d30d17d9
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-08-01 19:14:56 +0300

    Add navigation to reset pass page on first login

--- chama-dashboard/src/pages/Dashboard.jsx
+++ chama-dashboard/src/pages/Dashboard.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 import SmallWidget from "../components/DashboardComponents/SmallWidget";
 import DashStats from "../components/DashboardComponents/DashStats";
 import NewItems from "../components/NewItems";
 import MessageModal from "../modal/MessageModal";
 
 const Dashboard = () => {
+  const navigate = useNavigate();
+  const { userInfo } = useSelector((state) => state.member);
+
+  useEffect(() => {
+    if (userInfo?.isDefaultPass) {
+      navigate(`/members/${userInfo?.member_id}/set-password`);
+    }
+  });
   return (
     <div>
       <MessageModal />
